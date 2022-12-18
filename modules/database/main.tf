790f1db05f529a30885f4031d7135fcb804ddb1a
FelipeMenezesDM <contato@felipemenezes.com.br>
2022-12-18 01:34:18 -0300

    fix(module): ajuste para criação de bases de dados

--- modules/database/main.tf
+++ modules/database/main.tf
@@ -1,6 +1,5 @@
 # SQL database creation
 resource "google_sql_database" "sql_database" {
-  project   = var.project_name
   name      = var.database_name
   instance  = var.instance_name
   collation = var.database_collation
--- modules/database/variables.tf
+++ modules/database/variables.tf
@@ -1,7 +1,3 @@
-variable "project_name" {
-  type = string
-}
-
 variable "instance_name" {
   type = string
 }
