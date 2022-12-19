0f0455bc3d974e8f2ffc1e752b5484692fecb68d
FelipeMenezesDM <contato@felipemenezes.com.br>
2022-12-19 05:29:01 -0300

    feat(module): novo módulo para criação de subdomínios e atualização de documentação

---
+++ modules/domain/main.tf
@@ -0,0 +1,19 @@
+# Domain mapping creation
+resource "google_cloud_run_domain_mapping" "subdomain" {
+  project  = var.project_name
+  location = var.project_default_location
+  name     = "${var.subdomain}.${var.project_default_verified_domain}"
+
+  timeouts {
+    create = "20s"
+    delete = "20s"
+  }
+
+  metadata {
+    namespace = var.project_name
+  }
+
+  spec {
+    route_name = var.service_name
+  }
+}
---
+++ modules/domain/variables.tf
@@ -0,0 +1,19 @@
+variable "project_name" {
+  type = string
+}
+
+variable "project_default_location" {
+  type = string
+}
+
+variable "project_default_verified_domain" {
+  type = string
+}
+
+variable "subdomain" {
+  type = string
+}
+
+variable "service_name" {
+  type = string
+}
