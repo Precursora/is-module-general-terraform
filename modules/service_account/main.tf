a79905ccddc2f7cf6e3836109b71d2b91b95a035
FelipeMenezesDM <contato@felipemenezes.com.br>
2022-12-26 22:49:46 -0300

    feat(module): novo módulo para criação de contas de serviço

--- modules/service_account/main.tf
+++ modules/service_account/main.tf
@@ -8,5 +8,5 @@ resource "google_service_account_iam_binding" "service_account_iam_binding" {
   count               = length(var.roles)
   service_account_id  = google_service_account.service_account.name
   role                = var.roles[count.index]
-  members             = ["serviceAccount:${google_service_account.service_account.id}"]
+  members             = ["serviceAccount:${google_service_account.service_account.email}"]
 }
