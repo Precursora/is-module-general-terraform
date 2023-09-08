994da6ff262d6f308ef5c1e5f73cbc340246a605
Felipe Menezes <contato@felipemenezes.com.br>
2023-09-08 03:53:47 -0300

    feat: atualização de labels nos módulos

--- modules/instance/labels.tf
+++ modules/instance/labels.tf
@@ -1,13 +1,14 @@
 # Validate if default labels exists
 locals {
   valid_labels = {
-    owner-email   = var.labels.owner-email
-    tech-email    = var.labels.tech-email
-    team-email    = var.labels.team-email
-    support-email = var.labels.support-email
-    product       = var.labels.product
-    channel       = var.labels.channel
-    repository    = var.labels.repository
+    owner-email      = var.labels.owner-email
+    tech-email       = var.labels.tech-email
+    team-name        = var.labels.team-name
+    team-email       = var.labels.team-email
+    support-email    = var.labels.support-email
+    maintenance-code = var.labels.maintenance-code
+    product-name     = var.labels.product-name
+    repository       = var.labels.repository
   }
 }
 
