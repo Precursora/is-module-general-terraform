e3ed8ad84fe17ef60e77716916fd486bf9d821d3
Felipe Menezes <contato@felipemenezes.com.br>
2023-09-09 23:51:11 -0300

    feat: atualização de módulos de instância

--- modules/instance/variables.tf
+++ modules/instance/variables.tf
@@ -12,10 +12,12 @@ variable "vpc_id" {
 
 variable "default_region" {
   type = string
+  default = "us-central1"
 }
 
 variable "default_zone" {
   type = string
+  default = "us-central1-a"
 }
 
 variable "instance_name" {
