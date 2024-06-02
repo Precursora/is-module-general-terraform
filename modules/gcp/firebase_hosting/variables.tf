55c1ca7c7115990323c1833157ca9150415c847b
FelipeMenezesDM <contato@felipemenezes.com.br>
2024-06-02 06:35:22 -0300

    feat: criação de novo modulo para o firebase hosting

---
+++ modules/gcp/firebase_hosting/variables.tf
@@ -0,0 +1,19 @@
+variable "project_name" {
+  type = string
+}
+
+variable "website_domain" {
+  type = string
+}
+
+variable "website_subdomain" {
+  type = string
+}
+
+variable "cloudflare_zone_id" {
+  type = string
+}
+
+variable "cloudflare_provider_api_token" {
+  type = string
+}
