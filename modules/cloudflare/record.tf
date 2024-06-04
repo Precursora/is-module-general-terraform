5d7e616d2441024db5b35e329e97f750b2644cc4
FelipeMenezesDM <contato@felipemenezes.com.br>
2024-06-03 21:38:42 -0300

    feat: atualização de modulo de dominios

--- modules/cloudflare/record.tf
+++ modules/cloudflare/record.tf
@@ -1,13 +1,6 @@
 # Add a record to the domain
 resource "cloudflare_record" "record" {
-  zone_id = cond(
-              var.domain == "precursora.com.br",
-              var.cloudflare_provider_zone_id_precursoracombr,
-              cond(
-                var.domain == "felipemenezes.com.br",
-                var.cloudflare_provider_zone_id_felipemenezescombr
-              )
-            )
+  zone_id = var.domain == "precursora.com.br" ? var.cloudflare_provider_zone_id_precursoracombr : (var.domain == "felipemenezes.com.br" ? var.cloudflare_provider_zone_id_felipemenezescombr : "")
   name    = var.subdomain
   type    = var.record_type
   value   = var.record_value
