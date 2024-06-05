eefeb8c3d0c35429a8c64ace49b25c43993b1375
FelipeMenezesDM <contato@felipemenezes.com.br>
2024-06-05 12:17:18 -0300

    feat: teste de configuração de provider

--- modules/cloudflare/record.tf
+++ modules/cloudflare/record.tf
@@ -1,6 +1,6 @@
 # Add a record to the domain
 resource "cloudflare_record" "record" {
-  zone_id = lookup(data.github_actions_secrets.gh_secrets, (var.domain == "precursora.com.br" ? "CLOUDFLARE_ZONE_ID_PRECURSORACOMBR" : (var.domain == "felipemenezes.com.br" ? "CLOUDFLARE_ZONE_ID_FELIPEMENEZESCOMBR" : "")))
+  zone_id = lookup(data.github_actions_secrets.gh_secrets.secrets, (var.domain == "precursora.com.br" ? "CLOUDFLARE_ZONE_ID_PRECURSORACOMBR" : (var.domain == "felipemenezes.com.br" ? "CLOUDFLARE_ZONE_ID_FELIPEMENEZESCOMBR" : "")))
   name    = var.subdomain
   type    = var.record_type
   value   = var.record_value
