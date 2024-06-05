ed8e393b67a216ba271d61c95cfcf7ac606b9c92
FelipeMenezesDM <contato@felipemenezes.com.br>
2024-06-05 11:55:15 -0300

    feat: teste de configuração do provider

--- modules/cloudflare/provider.tf
+++ modules/cloudflare/provider.tf
@@ -8,5 +8,10 @@ terraform {
 }
 
 provider "cloudflare" {
-  api_token = data.github_actions_secrets.gh_secrets.CLOUDFLARE_API_TOKEN
+  api_token = data.github_actions_secrets.gh_secrets2.CLOUDFLARE_API_TOKEN
+}
+
+data "github_actions_secrets" "gh_secrets2" {
+  provider = github
+  name = "is-module-general-deploy-actions"
 }
