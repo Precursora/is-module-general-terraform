998c6b457d3cad041e6c8d7fbd1d2fe49ff1936e
FelipeMenezesDM <contato@felipemenezes.com.br>
2024-10-20 12:19:13 -0300

    fix: ajuste no provider do rediscloud

--- modules/rediscloud/provider.tf
+++ modules/rediscloud/provider.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    rediscloud = {
+      source = "RedisLabs/rediscloud"
+      version = "1.9.0"
+    }
+  }
+}
+
 provider "rediscloud" {
   api_key = data.external.env_var.result["rediscloud_api_key"]
   secret_key = data.external.env_var.result["rediscloud_secret_key"]
